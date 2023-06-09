e201efee5cefe48c4878f96a6e9c2be216a61902
1 <bogang2000@naver.com>
2023-06-09 10:02:43 +0900

    수정되라 제발

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,11 +1,14 @@
 package com.example.server.controller;
 
 
+import com.example.server.dto.Req;
 import com.example.server.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
@@ -16,7 +19,27 @@ public class ServerApiController {
         user.setName("steve");
         user.setAge(10);
         return user;
+    }
 
+    @PostMapping("/user/{userId}/name/{userName}")
+    public Req<User> post(
+            //HttpEntity<String> entity,
+            @RequestBody Req<User> user,
+            @PathVariable int userId,
+            @PathVariable String userName,
+            @RequestHeader("x-authorization") String authorization,
+            @RequestHeader("custom-header") String customHeader
+    ){
+        //log.info("req : {}", entity.getBody());
+        log.info("userId : {}, userName : {}", userId, userName);
+        log.info("authorization : {}, custom : {}", authorization, customHeader);
+        log.info("client req : {}", user);
 
+        Req<User> response = new Req<>();
+        response.setHeader(
+                new Req.Header()
+        );
+        response.setResBody(user.getResBody());
+        return response;
     }
 }
